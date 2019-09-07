f55bb92d39c3e509680eb779c49c91b2d461e5b3
Deepayan Patra <dpatra@andrew.cmu.edu>
2019-09-07 03:58:18 -0400

    transcribe successful for honey wav

---
+++ transcribe.py
@@ -0,0 +1,28 @@
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import speech
+from google.cloud.speech import enums
+from google.cloud.speech import types
+
+# Instantiates a client
+client = speech.SpeechClient()
+
+# The name of the audio file to transcribe
+file_name = "honeyaudio.wav"
+
+# Loads the audio into memory
+with io.open(file_name, 'rb') as audio_file:
+    content = audio_file.read()
+    audio = types.RecognitionAudio(content=content)
+
+config = types.RecognitionConfig(
+    audio_channel_count=2,
+    language_code='en-US')
+
+# Detects speech in the audio file
+response = client.recognize(config, audio)
+
+for result in response.results:
+    print('Transcript: {}'.format(result.alternatives[0].transcript))
